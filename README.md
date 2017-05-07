bbeb16eefb3d4680e504effa6621ccbd7fa57e99
Lauri Annala <lauri.annala.89@gmail.com>
2017-05-07 18:00:53 +0300

    Added cross-compiler toolset installation script

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 
-# noxOS
+# noxOS #
 Operating system for personal use.
+
+## Why? ##
+* Learning by designing and implementing:
+  * Kernel
+  * Unix OS and tools
+
+## Cross-compiler toolset ##
+* Pre-requisites:
+  * binuntils installed at: `$HOME/binutils-gdb`
+  * gcc installed at: `$HOME/gcc-7.1.0/configure`
+* Run `toolset_install.sh`
+* To be able to run tools with commands like `i686-elf-gcc --version`:
+  * Add location to PATH: `export PATH="$HOME/opt/cross/bin:$PATH"`
---
+++ toolset_install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Preparation
+export PREFIX="$HOME/opt/cross"
+export TARGET="i686-elf"
+export PATH="$PREFIX/bin:$PATH"
+
+# Binutils
+# Uses $HOME/binutils-gdb/configure
+cd $HOME/src
+mkdir build-binutils
+cd build-binutils
+../binutils-gdb/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+# GCC
+# Uses $HOME/gcc-7.1.0/configure
+cd $HOME/src
+which -- $TARGET-as || echo $TARGET-as is not in the PATH
+
+mkdir build-gcc
+cd build-gcc
+../gcc-7.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
